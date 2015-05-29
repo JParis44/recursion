d6e601341f0f1843741b4559176bcf43a491719c
Paris Caldwell <j.paris.caldwell@gmail.com>
2015-05-29 17:44:00 -0500

    Completed stringifyJSON

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,33 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  var objType = typeof obj;
+  var result = '';
+  
+  //Base cases
+  if (objType === 'string') {return '\"' + obj + '\"';}
+  if (objType === 'number' || objType === 'string' || objType === 'boolean') {
+    return obj.toString();
+  } else if (objType === 'object') {
+    if (obj === null) {return 'null';}
+    //Arrays (recursive)
+    if (Array.isArray(obj) === true) {
+      result = '';
+      obj.forEach(function (item, index, arr) {
+        result += stringifyJSON(item) + ',';                  //Recursion call
+      });
+      return '[' + result.slice(0, -1) + ']';
+    } else {
+      // Objects (recursive)
+      result = '';
+      for (var key in obj) {
+        if (!(typeof obj[key] === 'function' || obj[key] === undefined)) {
+          result += '\"' + key + '\":';
+          result += stringifyJSON(obj[key]) + ',';            //Recursion call
+        }
+      }
+      return '{' + result.slice(0, -1) + '}';
+    }
+  }
+  return '';
 };
